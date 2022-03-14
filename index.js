a488034d51e39de1cb81f97170e77f65751370ae
Rashedul Hasan <Rashedul Hasan>
2022-03-14 13:52:17 +0600

    Free-JS-Class-01

---
+++ index.js
@@ -0,0 +1,30 @@
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+// var mangoPrice = 5.345;
+// console.log(typeof mangoPrice);
+
+
+// var myName = "Rashedul";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+
+// var isBad = false;
+// console.log(typeof isBad);
+
+var myCountry = "I love Bangladesh";
+var update = myCountry.toLowerCase();
+console.log(update);
+
+var update2 = myCountry.toUpperCase();
+console.log(update2);
+
+var position = myCountry.indexOf("love");
+console.log(position);
+
+var part = myCountry.split(" ");
+console.log(part);
+
+
